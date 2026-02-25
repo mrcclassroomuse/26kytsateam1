39de5c1c6035e8c95f64f3415480be6697abc9b6
mrcclassroomuse <jared.cordray@jefferson.kyschools.us>
2026-02-25 15:33:13 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+console.log('will this work now?');
+console.log('dont forget to add script tags to all the html pages')
+/*
+From https://youtu.be/ifi6dXOl3g4?si=UUAk3P5_6_qTwbiZ
+Video where the searchbar's code is from
+
+
+*/
+const searchInput = document.getElementById('search');
+const clearSearch = document.getElementById('clearsearch');
+const clearForm = document.getElementById('clearform');
+clearSearch.onclick = function(){
+console.log("this doesn't clear the search bar like intended. \n it refreshes the page ");
+};
+searchInput.addEventListener('keyup', e=> {
+let currentValue = e.target.value.toLowerCase();
+console.log(currentValue);
+let resourceName = document.querySelectorAll('h1.resourcename');
+resourceName.forEach(resource =>{
+  if (resource.textContent.toLowerCase().includes(currentValue)){
+   resource.parentNode.parentNode.style.display = 'block';
+  } else {
+    resource.parentNode.parentNode.style.display = 'none';
+  }
+});
+});
+clearForm.onclick = function(){
+document.querySelectorAll('input[text]')
+
+}
